7960438de66de2869c1b8b8ee77924832899e43c
mbllt <mballet@student.42lyon.fr>
2022-02-10 22:13:29 +0100

    module07 ok

--- module07/ex02/includes/Array.hpp
+++ module07/ex02/includes/Array.hpp
@@ -7,31 +7,38 @@
 template<typename T>
 class Array {
 	private :
-		T *	_tab;
+		T *				_tab;
+		unsigned int	_n;
 
 	public :
-		Array() {_tab = new T;}
-		Array(unsigned int n) {_tab = new T[n];}
+		Array() : _n(0) {_tab = new T;_tab = NULL;}
+		Array(unsigned int n) : _n(n) {_tab = new T[n]; ft_bzero();}
 		Array(Array const & cpy) {*this = cpy;}
-		~Array();
+		~Array() {delete [] _tab;};
 
 		Array & operator=(Array const & src) {
 			_tab = new T[src.size()];
-
-			for (int i = 0; i < src.size(); i++)
+			_n = src._n;
+			for (unsigned int i = 0; i < _n; i++)
 				_tab[i] = src._tab[i];
 			return *this;
 			}
 		
-		Array & operator[](const unsigned int index) {
+		T & operator[](const unsigned int index) {
 			if (index >= this->size())
 				throw AccessorArrayInvalidExcpetion("Index to access array is invalid.");
 			return _tab[index];
 		}
 
-		unsigned int	size() const {
-			return sizeof(_tab)/sizeof(_tab[0]);
-			}
+		unsigned int	size() const {return _n;}
+
+		void	ft_bzero() {for (unsigned int i = 0; i < _n; i++) _tab[i] = 0;}
+
+		void	display(const unsigned int index) const {
+			if (index >= this->size())
+				throw AccessorArrayInvalidExcpetion("Index to access array is invalid.");
+			std::cout << _tab[index] << " ";
+		}
 
 		class AccessorArrayInvalidExcpetion : public std::exception {
 		
--- module07/ex02/srcs/main.cpp
+++ module07/ex02/srcs/main.cpp
@@ -2,16 +2,29 @@
 #include <string>
 #include "Array.hpp"
 
-// void	displayArray(Array &arr) {
-
-// }
+void	displayArray(Array<int> const & arr) {
+	for (unsigned int i = 0; i < arr.size(); i++)
+		arr.display(i);
+	std::cout << "\n";
+}
 
 int	main(void) {
 
-	// Array<int>	src(5);
+	Array<int>	src(5);
+	for (unsigned int i = 0; i < src.size(); i++)
+		src[i] = i;
+	std::cout << "src :" << "\n";
+	displayArray(src);
 
-	// src[0] = 1;
-	// Array<int>	dest(src);
+	std::cout << "dest copy of src :" << "\n";
+	Array<int>	dest(src);
+	displayArray(dest);
 
+	std::cout << "change src[0]" << "\n";
+	src[0] = 10;
+	std::cout << "src :" << "\n";
+	displayArray(src);
+	std::cout << "dest :" << "\n";
+	displayArray(dest);
 	return 0;
 }
